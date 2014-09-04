5533f77535c66d624540be2808b9029c64c1e716
Marco Fiscato <marco@swiftkey.net>
2014-09-04 14:57:22 +0100

    Added extra checks for directory existance.

--- observer/filelogger.lua
+++ observer/filelogger.lua
@@ -6,8 +6,8 @@
 local FileLogger, parent = torch.class("dp.FileLogger", "dp.Logger")
 FileLogger.isFileLogger = true
 
-function FileLogger:__init(save_dir)
-   self._save_dir = save_dir or dp.SAVE_DIR
+function FileLogger:__init(base_save_dir)
+   self._base_save_dir = base_save_dir or dp.SAVE_DIR
    parent.__init(self)
 end
 
@@ -15,14 +15,14 @@ function FileLogger:setup(config)
    parent.setup(self, config)
    --concatenate save directory with subject id
    local subject_path = self._subject:id():toPath()
-   self._save_dir = paths.concat(self._save_dir, subject_path)
+   self._save_dir = paths.concat(self._base_save_dir, subject_path)
    self._log_dir = paths.concat(self._save_dir, 'log')
-   --creates directories if required
-   os.execute('mkdir -p ' .. self._log_dir)
+   self:checkDir()
    print("FileLogger: log will be written to " .. self._log_dir)
 end
 
 function FileLogger:doneEpoch(report)
+  self:checkDir()
    self._last_epoch = report.epoch
    local filename = paths.concat(self._log_dir, 
                                  'report_' .. report.epoch .. '.dat')
@@ -32,6 +32,12 @@ function FileLogger:doneEpoch(report)
 end
 
 function FileLogger:logHyperReport(hyper_report)
+   self:checkDir()
    local filename = paths.concat(self._log_dir, 'hyper_report.dat')
    torch.save(filename, hyper_report)
+end
+
+function FileLogger:checkDir() 
+  --creates directories if required
+  os.execute('mkdir -p ' .. self._log_dir)
 end
